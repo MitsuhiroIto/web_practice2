8267be63f1965db5f65cce62e2d0d00052e2e611
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-02-17 04:05:37 +0900

    zappa-s3-docker_yolo-gpuのテスト完了

---
+++ zappa-s3-docker_yolo-gpu/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+import os
+import boto3
+import json
+
+app = Flask(__name__)
+bucket_name = 'mitsu-aws-image'
+app.config['USE_S3_DEBUG'] = True
+
+resource_s3 = boto3.resource('s3')
+client_s3 = boto3.client('s3')
+client_batch = boto3.client('batch')
+
+JOB_QUEUE = 'mitsu-8-6-yolo_queue'
+JOB_DEFINITION = 'mitsu-8-6-yolo_def'
+upload_folder = 'static/uploads/'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        upload_file = request.files['upload_file']
+        resource_s3.Bucket(bucket_name).put_object(Key= upload_folder + upload_file.filename, Body=upload_file.read(), ACL='public-read')
+        upload_url = 'https://s3-ap-northeast-1.amazonaws.com/' + bucket_name + '/' + upload_folder + upload_file.filename
+        client_s3.get_waiter('object_exists').wait(Bucket=bucket_name, Key=upload_folder + upload_file.filename)
+        return render_template('index.html', upload_url = upload_url )
+
+@app.route('/augmentation', methods=['GET', 'POST'])
+def augmentation():
+    if request.form['button_name'] == "YOLO":
+        f = open('batch_job.json', 'r')
+        containerOverrides = json.load(f)
+
+        containerOverrides['environment'][0]['value'] = file_name = request.form['upload_url'].rsplit('/', 1)[-1]
+        containerOverrides['environment'][1]['value'] = file_name_af = file_name.rsplit('.', 1)[0]   + "_yolo." + file_name.rsplit('.', 1)[1]
+        containerOverrides['environment'][2]['value'] = 's3://' + bucket_name + '/' + upload_folder + file_name
+        containerOverrides['environment'][3]['value'] = 's3://' + bucket_name + '/' + upload_folder + file_name_af
+        containerOverrides['command']=["sh","shell_script/detect_yolo-gpu/fetch_and_run.sh"]
+
+        client_batch.submit_job(
+            jobName='job-mitsu-' + datetime.now().strftime('%Y%m%d-%H%M%S'),
+            jobQueue=JOB_QUEUE,
+            jobDefinition=JOB_DEFINITION,
+            containerOverrides=containerOverrides
+            )
+
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
---
+++ zappa-s3-docker_yolo-gpu/create-batch-entities.py
@@ -0,0 +1,150 @@
+import boto3
+import argparse
+import time
+import sys
+
+batch = boto3.client('batch')
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--compute-environment", help="name of the compute environment", type=str, required=True)
+parser.add_argument("--subnets", help="comma delimited list of subnets", type=str, default='subnet-31146569')
+parser.add_argument("--security-groups", help="comma delimited list of security group ids",type=str, default='sg-10dbac69')
+parser.add_argument("--instance-role", help="instance role", type=str, default='arn:aws:iam::251699169519:instance-profile/ecsInstanceRole')
+parser.add_argument("--service-role", help="service role", type=str, default='arn:aws:iam::251699169519:role/service-role/AWSBatchServiceRole')
+parser.add_argument("--image-id", help="image id", type=str, default='ami-cf1b64a9')
+parser.add_argument("--key-pair", help="ec2 key pair", type=str, default='mitsu-aws2')
+args = parser.parse_args()
+
+spin = ['-', '/', '|', '\\', '-', '/', '|', '\\']
+
+def create_compute_environment(computeEnvironmentName, instanceType, unitVCpus, imageId, serviceRole, instanceRole,
+                               subnets, securityGroups, keyPair):
+    response = batch.create_compute_environment(
+        computeEnvironmentName=computeEnvironmentName,
+        type='MANAGED',
+        serviceRole=serviceRole,
+        computeResources={
+            'type': 'EC2',
+            'imageId': imageId,
+            'minvCpus': 0,
+            'maxvCpus': unitVCpus * 2,
+            'desiredvCpus': unitVCpus * 1,
+            'instanceTypes': instanceType,
+            'subnets': subnets,
+            'securityGroupIds': securityGroups,
+            'ec2KeyPair': keyPair,
+            'instanceRole': instanceRole
+        }
+    )
+
+    spinner = 0
+    while True:
+        describe = batch.describe_compute_environments(computeEnvironments=[computeEnvironmentName])
+        computeEnvironment = describe['computeEnvironments'][0]
+        status = computeEnvironment['status']
+        if status == 'VALID':
+            print('\rSuccessfully created compute environment %s' % (computeEnvironmentName))
+            break
+        elif status == 'INVALID':
+            reason = computeEnvironment['statusReason']
+            raise Exception('Failed to create compute environment: %s' % (reason))
+        print( '\rCreating compute environment... %s' % (spin[spinner % len(spin)])),
+        sys.stdout.flush()
+        spinner += 1
+        time.sleep(1)
+
+    return response
+
+
+def create_job_queue(computeEnvironmentName, jobQueueName):
+    response = batch.create_job_queue(jobQueueName=jobQueueName,
+                                      priority=0,
+                                      computeEnvironmentOrder=[
+                                          {
+                                              'order': 0,
+                                              'computeEnvironment': computeEnvironmentName
+                                          }
+                                      ])
+
+    spinner = 0
+    while True:
+        describe = batch.describe_job_queues(jobQueues=[jobQueueName])
+        jobQueue = describe['jobQueues'][0]
+        status = jobQueue['status']
+        if status == 'VALID':
+            print('\rSuccessfully created job queue %s' % (jobQueueName))
+            break
+        elif status == 'INVALID':
+            reason = jobQueue['statusReason']
+            raise Exception('Failed to create job queue: %s' % reason)
+        print ('\rCreating job queue... %s' % (spin[spinner % len(spin)])),
+        sys.stdout.flush()
+        spinner += 1
+        time.sleep(1)
+
+    return response, jobQueueName
+
+def register_job_definition(jobDefName, image, unitVCpus, unitMemory):
+    response = batch.register_job_definition(jobDefinitionName=jobDefName,
+                                             type='container',
+                                             containerProperties={
+                                                 'image': image,
+                                                 'vcpus': unitVCpus,
+                                                 'memory': unitMemory,
+                                                 'privileged': True,
+                                                 'volumes': [
+                                                     {
+                                                         'host': {
+                                                             'sourcePath': '/var/lib/nvidia-docker/volumes/nvidia_driver/latest'
+                                                         },
+                                                         'name': 'nvidia-driver-dir'
+                                                     }
+                                                 ],
+                                                 'mountPoints': [
+                                                     {
+                                                         'containerPath': '/usr/local/nvidia',
+                                                         'readOnly': True,
+                                                         'sourceVolume': 'nvidia-driver-dir'
+                                                     }
+                                                 ]
+                                             })
+    print ('Created job definition %s' % response['jobDefinitionName'])
+    return response
+
+def main():
+    computeEnvironmentName = args.compute_environment
+    jobQueueName = computeEnvironmentName + '_queue'
+    jobDefName = computeEnvironmentName + '_def'
+
+    imageId = args.image_id
+    serviceRole = args.service_role
+    instanceRole = args.instance_role
+    subnets = args.subnets.split(",")
+    securityGroups = args.security_groups.split(",")
+    keyPair = args.key_pair
+
+    # vcpus and memory in a p2.xlarge
+    unitVCpus = 4
+    unitMemory = 61000
+
+    create_compute_environment(computeEnvironmentName=computeEnvironmentName,
+                               instanceType=['p2.xlarge', 'p2.8xlarge', 'p2.16xlarge'],
+                               unitVCpus=4,
+                               imageId=imageId,
+                               serviceRole=serviceRole,
+                               instanceRole=instanceRole,
+                               subnets=subnets,
+                               securityGroups=securityGroups,
+                               keyPair=keyPair)
+
+    create_job_queue(computeEnvironmentName, jobQueueName)
+
+    container_url = '251699169519.dkr.ecr.ap-northeast-1.amazonaws.com/mitsuhiro3116/yolo_detect-gpu-8-6-nvidia'
+    register_job_definition(jobDefName=jobDefName, image=container_url, unitVCpus=unitVCpus, unitMemory=unitMemory)
+
+    print('Successfully created batch entities (compute environment: {}, job queue: {}, job definition: {})'.format(computeEnvironmentName, jobQueueName, jobDefName))
+
+
+if __name__ == "__main__":
+    main()
